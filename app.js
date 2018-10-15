0f1073c79510757224b95ed76bbfcd7bdb2ef886
Kevin <kevanatole@gmail.com>
2018-10-15 21:10:03 +0100

    Read a command from the command line

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ console.log('Starting app.js')
 const fs = require('fs')
 const _ = require('lodash')
 const notes = require('./notes')
+
+const command = process.argv[2]
+console.log('Command: ', command)
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');
+} if (command === 'read') {
+    console.log('Reading a note');
+
+} else if (command === 'remove') {
+    console.log('Removing a note');
+
+} else {
+    console.error('Command not recognized');
+}
+
