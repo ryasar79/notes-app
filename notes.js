f385a7841a82aa8f090e82b84138e8e8fc5cf931
Kevin ANATOLE <kevin@kevinanatole.uk>
2018-10-21 13:35:17 +0100

    Reading the notes from the file. Checking for duplicates.

--- notes.js
+++ notes.js
@@ -1,24 +1,46 @@
-console.log('Starting notes.js')
+console.log('Starting notes.js');
+
+const fs = require('fs');
 
 const addNote =  (title, body) => {
-    console.log('Adding note: ', title, body)
-}
+    var notes = [];
+    const note = {
+        title,
+        body
+    };
+
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+    }
+
+    var duplicateNotes = notes.filter((note) => {
+        return note.title  === title;
+    });
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
+};
 
 const getAll = () => {
-    console.log('Getting all notes')
-}
+    console.log('Getting all notes');
+};
 
 const read = (title) => {
-    console.log(`Reading note with title ${title}`)
-}
+    console.log(`Reading note with title ${title}`);
+};
 
 const remove = (title) => {
-    console.log(`Remove note with title ${title}`)
-}
+    console.log(`Remove note with title ${title}`);
+};
 
 module.exports = {
     addNote,
     getAll,
     read,
     remove
-}
+};
+
